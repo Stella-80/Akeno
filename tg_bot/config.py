919dec0b41f364dd976e4988a08f90ce152cad52
Stella-80 <82025071+Stella-80@users.noreply.github.com>
2021-04-27 00:51:01 +0530

    Create config.py

---
+++ tg_bot/config.py
@@ -0,0 +1,37 @@
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "1700341854:AAHfauOgEqtNpem6d9KSHkhixfzR4lI__NU"
+    OWNER_ID = "1461968113" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "watts_on_xd"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'postgres://stella:stella123@database-1.cuzno6euao1o.us-east-2.rds.amazonaws.com:5431/dbname'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = True
+    STRICT_GMUTE = True
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    ALLOW_EXCL = False  # Allow ! commands as well as /
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
