bed6ce2003bb14fb543bfad722ab92bf96e35370
Stella-80 <82025071+Stella-80@users.noreply.github.com>
2021-04-27 11:57:21 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -2,7 +2,7 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "1700341854:AAEZnftr9eodgblXNzYicdA6ybWbwCzqGWE"
+    API_KEY = "1700341854:AAEXIDuCUCtwPd4cUGyqYvMYA2NyleHZ6FM"
     OWNER_ID = "1461968113" # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "watts_on_xd"
 
