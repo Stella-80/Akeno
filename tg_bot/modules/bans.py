a74b1d9b25fce2ba701afc47f6ec60ea57706e4d
Stella-80 <82025071+Stella-80@users.noreply.github.com>
2021-04-29 22:48:51 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -89,7 +89,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
 
     try:
         chat.kick_member(user_id)
-        bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
+        bot.(chat.id,) 
         keyboard = []
         reply = "{} Banned!".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
